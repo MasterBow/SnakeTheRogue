cd4458b139e0aef71396a274d0000f0321c43009
MasterBow <MasterBow@outlook.es>
2025-11-14 21:50:54 -0800

    Create config.py
    (Contiene todas las constantes. Si quieres cambiar un color o el tamaño del mundo, lo haces aquí).

---
+++ src/config.py
@@ -0,0 +1,21 @@
+# config.py
+
+# --- Dimensiones ---
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WORLD_WIDTH = 2000
+WORLD_HEIGHT = 2000
+
+# --- Colores ---
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (211, 47, 47)
+GREEN = (76, 175, 80)
+BLUE = (33, 150, 243)
+YELLOW = (255, 235, 59)
+PURPLE = (156, 39, 176)
+ORANGE = (255, 152, 0)
+
+# --- Configuración del Juego ---
+SNAKE_SIZE = 20
+SNAKE_SPEED = 10
